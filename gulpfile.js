e2ae29458550b77f719b14d369c0afb422ddd943
Wayne Austin <Wayne Austin>
2015-11-04 07:06:11 +0000

    Set up of gulp tasks. Updated npm and bower configs.

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Wayne on 04/11/2015.
+ */
+var gulp = require('gulp'),
+    plugins = require('gulp-load-plugins')({
+      pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+      replaceString: /\bgulp[\-.]/
+    });
+
+gulp.task('less', function () {
+  return gulp.src('styles/*.less')
+    .pipe(plugins.less())
+    .pipe(plugins.minifyCss())
+    .pipe(gulp.dest('styles/'));
+});
+
+gulp.task('libs', function () {
+  return gulp.src(plugins.mainBowerFiles().concat())
+    .pipe(plugins.filter('*.js'))
+    .pipe(gulp.dest('libs/'));
+});
+
+gulp.task('default', ['less', 'libs']);
---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Wayne on 04/11/2015.
+ */
+/// <reference path="/libs/angular.js" />
+
+(function () {
+
+  var app = angular.module("app")
+
+}());
