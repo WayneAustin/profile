502881fb7331029eead568538b29369d0777a1fa
Wayne Austin <Wayne Austin>
2015-11-05 07:16:14 +0000

    Integrated firebase and angular. App and controller setup.

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,19 @@
 
 (function () {
 
-  var app = angular.module("app")
+  var app = angular.module("app", ['firebase', 'controllers', 'directives', 'services']);
+
+  angular.module("controllers", []);
+  angular.module("directives", []);
+  angular.module("services", []);
+
+  angular.module("controllers")
+    .controller("MasterController", function ($scope, $firebaseObject, $firebaseArray) {
+      var fbRef = new Firebase("https://blistering-fire-7593.firebaseio.com");
+
+      $scope.data = $firebaseObject(fbRef);
+
+
+    })
 
 }());
